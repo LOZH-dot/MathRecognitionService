b45f8c6607f4fd267371dda13757b3911b9155cb
Salieri <Salieri@DESKTOP-QL85CJN>
2024-04-02 13:33:17 +0300

    Добавлены скрипты обучения нейронной сети, сегментации изображения, предсказания класса символа

---
+++ RecognitonModule/predict.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import keras
+import numpy as np
+
+def predict(image_path):
+    model = keras.models.load_model('model.keras')
+    img = tf.keras.utils.load_img(
+        image_path, target_size=(45, 45)
+    )
+    class_names = ['!', '(', ')', '+', ',', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', 'A', 'C',
+                       'Delta',
+                       'G', 'H', 'M', 'N', 'R', 'S', 'T', 'X', '[', ']', 'alpha', 'ascii_124', 'b', 'beta', 'cos', 'd',
+                       'div',
+                       'e', 'exists', 'f', 'forall', 'forward_slash', 'gamma', 'geq', 'gt', 'i', 'in', 'infty', 'int',
+                       'j', 'k',
+                       'l', 'lambda', 'ldots', 'leq', 'lim', 'log', 'lt', 'mu', 'neq', 'o', 'p', 'phi', 'pi', 'pm',
+                       'prime',
+                       'q', 'rightarrow', 'sigma', 'sin', 'sqrt', 'sum', 'tan', 'theta', 'times', 'u', 'v', 'w', 'y',
+                       'z', '{',
+                       '}']
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+
+    return class_names[np.argmax(score)]
+    # print(
+    #     "This image most likely belongs to {} with a {:.2f} percent confidence."
+    #     .format(class_names[np.argmax(score)], 100 * np.max(score))
+    # )
